a980cd17145d34d18cc3ed023426aea4d159bad2
yerti <yerti0818@gmail.com>
2023-06-19 13:22:25 -0700

    Write a C program that prints text with the puts function

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: programming in c
+ *
+ * Return: alwas 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
