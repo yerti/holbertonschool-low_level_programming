6ce0cc32e60086723180d24119e1b272d1c21994
yerti <yerti0818@gmail.com>
2023-06-21 16:22:26 -0700

    Write a function that prints the alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -first program
+ *
+ * Description: writing functions
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	/* a = alphabet*/
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a)
+	}
+
+	_putchar('\n');
+	return (0);
+}
