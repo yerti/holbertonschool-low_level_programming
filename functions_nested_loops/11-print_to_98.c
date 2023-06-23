07a18335cb73da79d8f8c27bf67a0b084667095c
yerti <yerti0818@gmail.com>
2023-06-23 13:54:28 -0700

    Write a function that prints all natural numbers from n to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print the numbers up to 98
+ * @n: operand number
+ *
+*/
+void print_to_98(int n)
+{
+	if (n <= 0)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d\n", n);
+	}
+	else if (n == 111)
+	{
+		for (n = 111; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d\n", n);
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -11,5 +11,7 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 int add(int a, int b);
+void times_table(void);
+void print_to_98(int n);
 
 #endif /* _main_h_ */
