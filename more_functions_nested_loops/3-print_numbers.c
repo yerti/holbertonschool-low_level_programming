99ed52ca7b20baf51cbf0152449ea878e9e516a3
yerti <yerti0818@gmail.com>
2023-06-24 08:34:41 -0700

    print the numbers from 1 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers from 1 to 9
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');
+	}
+
+	_putchar('\n');
+
+}
