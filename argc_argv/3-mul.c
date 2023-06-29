afc18c48dda710346f02495c7a5faf9470bac43f
yerti <yerti0818@gmail.com>
2023-06-28 18:56:45 -0700

    print the multiplication of two numbers

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,11 +11,12 @@
  */
 int main(int argc, char *argv[])
 {
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
+	int a, b;
 
 	if (argc == 3)
 	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
 		printf("%d\n", a * b);
 	}
 	else
