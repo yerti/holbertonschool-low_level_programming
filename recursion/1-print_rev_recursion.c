81251ad8873c341e504efc0b1d9ab57b3265f65a
yerti <yerti0818@gmail.com>
2023-06-29 06:31:20 -0700

    prints a string in reverse

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * _print_rev_recursion - prints a string in reverse
+ * @s: first parameter
  */
 void _print_rev_recursion(char *s)
 {
 	int carc = 0;
+
 	if (s[carc] != '\0')
 	{
 		_print_rev_recursion(s + 1);
