465437640851b639cbf85cc732febf78bdb14b65
yerti <yerti0818@gmail.com>
2023-06-30 14:31:48 -0700

    copies the string pointed to by src

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: first parameter
+ * @src: second parameter
+ * 
+ * Return: the parameter dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return dest;
+}	
