a8248d87eb85623477aeaf2ee6c20917c52c6d9b
yerti <yerti0818@gmail.com>
2023-07-01 14:55:30 -0700

    returns the value of a power

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursionint - print the numbe x to the power of and
+ * @x: power base
+ * @y: power exponent
+ * Return: power result
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	return (x * _pow_recursion(x, y -1));
+}
