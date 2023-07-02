ad292749cce1ff068cd3a092c94c23acc124991f
yerti <yerti0818@gmail.com>
2023-07-02 14:17:47 -0700

    a pointer to the duplicated string

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - devuelva un puntero a un espacio recién asignado en la memoria
+ * @str: text string
+ * Return: a pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	unsigned int i = 0;
+	unsigned int n = 0;
+	char *y = malloc(sizeof(char) * (i + 1));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[i])
+		i++;
+
+	if (y == NULL)
+		return (NULL);
+
+	while (str[n])
+	{
+		y[n] = str[n];
+		n++;
+	}
+
+	y[n + 1] = 0;
+	return (y);
+}
+
