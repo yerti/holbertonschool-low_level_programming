e29729622b93688d529da22b348461bd95c8d253
yerti <yerti0818@gmail.com>
2023-07-03 13:44:48 -0700

    new name for the type

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @age: how old is the dog
  * @owner: owner the dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
