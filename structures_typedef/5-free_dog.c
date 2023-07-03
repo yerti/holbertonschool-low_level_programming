05477eb7690e5c6cd600995381a0eceaa68caa2f
yerti <yerti0818@gmail.com>
2023-07-03 16:35:23 -0700

    function that frees dogs

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * fre_dog - function that frees dogs
+ * @d: first parameter
+ */
+void free_dog(dog_t *d)
+{
+        if (d)
+        {
+                free(d->name);
+                free(d->owner);
+                free(d);
+        }
+}
