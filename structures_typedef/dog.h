b5c86bb0eb9ad658fbd1e5ace6045bfedef12d97
yerti <yerti0818@gmail.com>
2023-07-03 10:54:15 -0700

    function that creates a structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _dog_h_
+#define _dog_h_
+#include <stdio.h>
+
+/**
+ * struct dog - function containing a structure
+ * @name: name the dog
+ * @age: how old is the dog
+ * @owner: owner the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* dog.h */
