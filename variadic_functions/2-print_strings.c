ea20d738b1c681f9245269a7173f4559362454bc
yerti <yerti0818@gmail.com>
2023-07-05 16:00:38 -0700

    task02

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ *
+ *
+ *
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	const char *alm;
+	
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		alm = va_arg(ap, const char *);
+		if (alm != NULL) 
+		{
+			printf("%s", alm);
+		}
+		else
+		{
+			printf("nil");
+		}
+
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(ap);
+}
+
