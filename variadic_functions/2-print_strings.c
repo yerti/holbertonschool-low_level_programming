99cf80dbdffb33b85d64e16debd93240c9511f9a
yerti <yerti0818@gmail.com>
2023-07-05 16:06:47 -0700

    task02

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -3,28 +3,29 @@
 #include "variadic_functions.h"
 
 /**
- *
- *
- *
+ * print_strings - prints strings, followed by a new line
+ * @separator: prints the comma
+ * @n: the numbers
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
 	const char *alm;
-	
+
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
 		alm = va_arg(ap, const char *);
-		if (alm != NULL) 
+
+		if (alm != NULL)
 		{
 			printf("%s", alm);
 		}
 		else
 		{
-			printf("nil");
+			printf("(nil)");
 		}
 
 		if (separator != NULL && i < n - 1)
